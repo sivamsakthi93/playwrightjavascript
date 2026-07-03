9b73fa4e7010d8041e757d1348b5b08f05693468
sivamsakthi93 <sivamsakthi93@gmail.com>
2026-07-03 10:04:04 +0530

    ccc

---
+++ string.js
@@ -0,0 +1,15 @@
+let a ="javascript";
+let b = new String("javascript");
+let c = new String(a);
+
+//length property is used to get the length of a string
+console.log(a.length);
+
+//indexOf() method is used to get the index of a string
+console.log(a.indexOf("j"));
+
+//concat() method is used to concatenate two strings
+console.log(a.concat(" is a programming language"));
+
+//includes() method is used to check if a string contains a specified value
+console.log(a.includes("java"));
